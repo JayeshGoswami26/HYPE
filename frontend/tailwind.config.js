27c24297bb6a3a0f0b34d87e752c99b64b62808e
Jayesh Goswami <goswamijayesh54@gmail.com>
2025-01-24 00:01:52 +0530

    error fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -44,3 +43,4 @@ export default {
     ],
   },
 };
+
